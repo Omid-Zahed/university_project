c514c46b363ae43c7d37d60c14d4c14af4c7196c
omid-zahed <omid98.ir@gmail.com>
2022-01-25 01:36:47 +0330

    i

--- simple_student_management/src/Main.java
+++ simple_student_management/src/Main.java
@@ -3,18 +3,17 @@ import app.Core;
 public class Main {
 
     public static void main(String[] args) {
-       Core App= new Core();
-       App.init();
-       while (true) {
-           App.run();
-           try{}
+           try{
+               Core App= new Core();
+               App.init();
+               App.run();
+           }
            catch (Exception e){
               System.err.println("sorry something went wrong");
               System.err.println("Please try again");
               System.err.println("Error: "+e.getMessage());
            }
-       }
-
+            main(args);
 
     }
 
